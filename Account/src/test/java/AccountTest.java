57bf23f8f7038817c7304b9a12b980090319e8a9
Gitta <birgitta.oberg@metropolia.fi>
2024-09-10 22:43:30 +0300

    fix

--- Account/src/test/java/AccountTest.java
+++ Account/src/test/java/AccountTest.java
@@ -1,9 +1,24 @@
+import org.junit.AfterClass;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class AccountTest {
     Account account = new Account();
+
+    @BeforeClass
+    public static void setUp() {
+        System.out.println("Test starts");
+    }
+
+    @Before
+    public void setUpBeforeTests() {
+        System.out.println("Before each test");
+        account.deposit(0);
+    }
 
     @Test
     public void testDeposit() {
@@ -21,5 +36,15 @@ public class AccountTest {
     public void testGetBalance() {
         account.deposit(500);
         assertEquals(500, account.getBalance(), 0.05);
+    }
+
+    @After
+    public void cleanUpAfterTests() {
+        System.out.println("After each test");
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        System.out.println("End the test");
     }
 }
